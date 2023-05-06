827d7580533060a2e83adfbee3820c51b44f3438
Ivarchow <89149103+Ivarchow@users.noreply.github.com>
2023-05-06 00:02:10 -0600

    Delete HM3 directory

--- HM3/exercise1/VenueBO.java
+++
@@ -1,9 +0,0 @@
-package domain.hcl.homeworks.HM3.exercise1;
-
-public class VenueBO {
-
-	void displayVenueDetails(Venue venue) {
-		System.out.println(
-				"Venue Details"+"\n"+venue.getName()+","+venue.getCity());
-	}
-}
